f883961a8c68c5c3d353c298127f7dacc5e1382a
Vlad Taranov <wvlad.dev@gmail.com>
2016-01-14 01:01:26 +0200

    platforms: * Silverlight 4 now has FEAT_COMPILER; * Windows Phone 7 platform removed (but Portable for WP7 is still there); * Unity renamed to net20safe+unity; * added net30safe; - mono 2.6.7 removed, use net20/net20safe+unity/net30/net30safe versions instead;
    also removed some old files and configurations;
    
    referenced RunSharp.

--- build_gmcs.bat
+++
@@ -1,9 +0,0 @@
-@echo Building Full...
-@rd /s /q Package\Full\mono
-@md Package\Full\mono
-@call mcs -recurse:protobuf-net\*.cs -out:Package\Full\mono\aqlaserializer.dll -target:library -unsafe+ -define:FEAT_COMPILER -define:PLAT_BINARYFORMATTER -doc:Package\Full\mono\protobuf-net.xml -define:FEAT_SERVICEMODEL -define:PLAT_XMLSERIALIZER -r:System.Runtime.Serialization.dll -r:System.ServiceModel.dll -r:System.Configuration.dll /keyfile:AqlaSerializerKey.snk
-
-@echo Building CoreOnly...
-@rd /s /q Package\CoreOnly\mono
-@md Package\CoreOnly\mono
-@call mcs -recurse:protobuf-net\*.cs -out:Package\CoreOnly\mono\aqlaserializer.dll -target:library -unsafe+ -define:PLAT_BINARYFORMATTER -doc:Package\CoreOnly\mono\protobuf-net.xml -define:FEAT_SERVICEMODEL -define:PLAT_XMLSERIALIZER -r:System.Runtime.Serialization.dll -r:System.ServiceModel.dll -r:System.Configuration.dll -define:NO_RUNTIME /keyfile:AqlaSerializerKey.snk
--- buildpack.cmd
+++ buildpack.cmd
@@ -1,3 +1,3 @@
-@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild all.build
+@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild all.build /p:WarningLevel=0
 @packages\NuGet.CommandLine.2.0.40000\tools\NuGet.exe pack Nuget\aqlaserializer.nuspec
 pause
