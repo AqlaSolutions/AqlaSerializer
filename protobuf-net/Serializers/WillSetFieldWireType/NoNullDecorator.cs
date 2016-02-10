f21ab5015dd485067530aeec8b43d1317951e3ea
Vlad Taranov <wvlad.dev@gmail.com>
2016-02-10 15:09:20 +0200

    nonull decorator

--- protobuf-net/Serializers/WillSetFieldWireType/NoNullDecorator.cs
+++ protobuf-net/Serializers/WillSetFieldWireType/NoNullDecorator.cs
@@ -1,14 +1,15 @@
 ﻿// Modified by Vladyslav Taranov for AqlaSerializer, 2016
+
 #if !NO_RUNTIME
 using System;
-
+using System.Diagnostics;
 using AqlaSerializer.Meta;
-
 #if FEAT_IKVM
 using Type = IKVM.Reflection.Type;
 using IKVM.Reflection;
 #else
 using System.Reflection;
+
 #endif
 
 namespace AqlaSerializer.Serializers
@@ -21,8 +22,9 @@ namespace AqlaSerializer.Serializers
         public bool DemandWireTypeStabilityStatus() => !_throwIfNull;
         readonly bool _throwIfNull;
         private readonly Type expectedType;
-        
-        public NoNullDecorator(TypeModel model, IProtoSerializerWithWireType tail, bool throwIfNull) : base(tail)
+
+        public NoNullDecorator(TypeModel model, IProtoSerializerWithWireType tail, bool throwIfNull)
+            : base(tail)
         {
             _throwIfNull = throwIfNull;
             Type tailType = tail.ExpectedType;
@@ -41,27 +43,18 @@ namespace AqlaSerializer.Serializers
 
         }
 
-        public override Type ExpectedType
-        {
-            get { return expectedType; }
-        }
-        public override bool ReturnsValue
-        {
-            get { return true; }
-        }
-        public override bool RequiresOldValue
-        {
-            get { return true; }
-        }
+        public override Type ExpectedType { get { return expectedType; } }
+        public override bool ReturnsValue { get { return true; } }
+        public override bool RequiresOldValue { get { return true; } }
 
 #if !FEAT_IKVM
         public override object Read(object value, ProtoReader source)
         {
             return Tail.Read(value, source);
         }
+
         public override void Write(object value, ProtoWriter dest)
         {
-            // TODO emit
             if (value != null)
             {
                 Tail.Write(value, dest);
@@ -76,34 +69,55 @@ namespace AqlaSerializer.Serializers
 #if FEAT_COMPILER
         protected override void EmitRead(Compiler.CompilerContext ctx, Compiler.Local valueFrom)
         {
-            using (Compiler.Local oldValue = ctx.GetLocalWithValue(expectedType, valueFrom))
+            using (Compiler.Local oldValue = ctx.GetLocalWithValueForEmitRead(this, valueFrom))
             {
+                Compiler.Local tempLocal = null;
+                Compiler.Local valueForTail;
+                Debug.Assert(!Tail.RequiresOldValue || Tail.ReturnsValue);
                 if (Tail.RequiresOldValue)
                 {
                     if (expectedType.IsValueType)
                     {
+                        tempLocal = ctx.Local(Tail.ExpectedType);
                         ctx.LoadAddress(oldValue, expectedType);
                         ctx.EmitCall(expectedType.GetMethod("GetValueOrDefault", Helpers.EmptyTypes));
+                        ctx.StoreValue(tempLocal);
+                        valueForTail = tempLocal;
                     }
-                    else
-                    {
-                        ctx.LoadValue(oldValue);
-                    }
+                    else valueForTail = oldValue;
                 }
-                Tail.EmitRead(ctx, null);
-                // note we demanded always returns a value
+                else valueForTail = null;
+
+                // valueForTail contains:
+                // null: when not required old value
+                // oldValue local: when reference type
+                // tempLocal: when nullable value type
+
+                Tail.EmitRead(ctx, valueForTail);
+
                 if (expectedType.IsValueType)
                 {
+                    if (!Tail.ReturnsValue)
+                        ctx.LoadValue(tempLocal);
+                    // note we demanded always returns a value
                     ctx.EmitCtor(expectedType, Tail.ExpectedType); // re-nullable<T> it
                 }
-                ctx.StoreValue(oldValue);
+
+                if (Tail.ReturnsValue || expectedType.IsValueType)
+                    ctx.StoreValue(oldValue);
+
+                if (ReturnsValue)
+                    ctx.LoadValue(oldValue);
+
+                if (!tempLocal.IsNullRef()) tempLocal.Dispose();
             }
         }
+
         protected override void EmitWrite(Compiler.CompilerContext ctx, Compiler.Local valueFrom)
         {
-            using(Compiler.Local valOrNull = ctx.GetLocalWithValue(expectedType, valueFrom))
+            using (Compiler.Local valOrNull = ctx.GetLocalWithValue(expectedType, valueFrom))
             {
-                
+
                 if (expectedType.IsValueType)
                 {
                     ctx.LoadAddress(valOrNull, expectedType);
@@ -113,23 +127,38 @@ namespace AqlaSerializer.Serializers
                 {
                     ctx.LoadValue(valOrNull);
                 }
-                Compiler.CodeLabel @end = ctx.DefineLabel();
-                ctx.BranchIfFalse(@end, false);
-                if (expectedType.IsValueType)
-                {
-                    ctx.LoadAddress(valOrNull, expectedType);
-                    ctx.EmitCall(expectedType.GetMethod("GetValueOrDefault", Helpers.EmptyTypes));
-                }
-                else
-                {
-                    ctx.LoadValue(valOrNull);
-                }
-                Tail.EmitWrite(ctx, null);
+                Compiler.CodeLabel done = ctx.DefineLabel();
+                Compiler.CodeLabel onNull = ctx.DefineLabel();
 
-                ctx.MarkLabel(@end);
+                ctx.BranchIfFalse(onNull, false);
+                // if !=null
+                {
+                    if (expectedType.IsValueType)
+                    {
+                        ctx.LoadAddress(valOrNull, expectedType);
+                        ctx.EmitCall(expectedType.GetMethod("GetValueOrDefault", Helpers.EmptyTypes));
+                    }
+                    else
+                    {
+                        ctx.LoadValue(valOrNull);
+                    }
+                    Tail.EmitWrite(ctx, null);
+
+                    ctx.Branch(done, true);
+                }
+                // else
+                {
+                    ctx.MarkLabel(onNull);
+                    if (_throwIfNull)
+                        ctx.G.ThrowNullReferenceException();
+                    else
+                        ctx.G.Writer.WriteFieldHeaderCancelBegin();
+                }
+                ctx.MarkLabel(done);
             }
         }
 #endif
     }
 }
+
 #endif
