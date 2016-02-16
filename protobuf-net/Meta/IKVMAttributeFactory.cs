fceadfdda86b6cd21d2c90c309669e2bd1891935
Vlad Taranov <wvlad.dev@gmail.com>
2016-02-16 12:25:13 +0200

    ReflectionObjectMap to create attribute on IKVM representation

---
+++ protobuf-net/Meta/IKVMAttributeFactory.cs
@@ -0,0 +1,40 @@
+﻿#if !NO_RUNTIME && FEAT_IKVM
+using System;
+using System.Reflection;
+using AltLinq;
+
+namespace AqlaSerializer.Meta
+{
+    static class IKVMAttributeFactory
+    {
+        public static object Create(IKVM.Reflection.CustomAttributeData attribute)
+        {
+            var systemType = System.Type.GetType(attribute.Constructor.DeclaringType.FullName, true);
+            var obj = Activator.CreateInstance(systemType, attribute.ConstructorArguments.Select(a => a.Value).ToArray());
+
+            var members = Helpers.GetInstanceFieldsAndProperties(systemType, false).ToDictionary(m => m.Name, StringComparer.Ordinal);
+            var membersIgnoreCase = Helpers.GetInstanceFieldsAndProperties(systemType, false).ToDictionary(m => m.Name, StringComparer.OrdinalIgnoreCase);
+            
+            foreach (var arg in attribute.NamedArguments)
+            {
+                MemberInfo member;
+                if (!members.TryGetValue(arg.MemberInfo.Name, out member))
+                if (!membersIgnoreCase.TryGetValue(arg.MemberInfo.Name, out member))
+                {
+                    continue;
+                }
+
+                PropertyInfo prop = member as PropertyInfo;
+                if (prop != null)
+                    Helpers.GetSetMethod(prop, true, true).Invoke(obj, new[] { arg.TypedValue.Value });
+
+                FieldInfo field = member as FieldInfo;
+                field?.SetValue(obj, new[] { arg.TypedValue.Value });
+            }
+
+            return obj;
+        }
+    }
+}
+
+#endif
---
+++ protobuf-net/Meta/ReflectionObjectMap.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+
+namespace AqlaSerializer.Meta
+{
+    sealed class ReflectionObjectMap
+    {
+        public object Target { get; }
+        readonly MemberInfo[] _membersPublic;
+        readonly MemberInfo[] _membersAll;
+
+        public bool TryGet(string key, bool publicOnly, out object value)
+        {
+            foreach (MemberInfo member in publicOnly ? _membersPublic : _membersAll)
+            {
+#if FX11
+                if (member.Name.ToUpper() == key.ToUpper())
+#else
+                if (string.Equals(member.Name, key, StringComparison.OrdinalIgnoreCase))
+#endif
+                {
+                    PropertyInfo prop = member as PropertyInfo;
+                    if (prop != null)
+                    {
+                        value = Helpers.GetPropertyValue(prop, Target);
+                        return true;
+                    }
+                    FieldInfo field = member as FieldInfo;
+                    if (field != null)
+                    {
+                        value = field.GetValue(Target);
+                        return true;
+                    }
+
+                    throw new NotSupportedException(member.GetType().Name);
+                }
+            }
+            value = null;
+            return false;
+        }
+        
+        public ReflectionObjectMap(object target)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            Target = target;
+            _membersPublic = Helpers.GetInstanceFieldsAndProperties(target.GetType(), true);
+            try
+            {
+                _membersAll = Helpers.GetInstanceFieldsAndProperties(target.GetType(), false);
+            }
+            catch
+            {
+                _membersAll = _membersPublic;
+            }
+        }
+    }
+}
