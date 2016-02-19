1c27b7631262ea83d90366a7f3250467d0b1b097
Vlad Taranov <wvlad.dev@gmail.com>
2016-02-19 17:04:32 +0200

    build

--- protobuf-net.unittest/Aqla/Issue7ListHandlingCallbacksProto.cs
+++ protobuf-net.unittest/Aqla/Issue7ListHandlingCallbacksProto.cs
@@ -22,6 +22,9 @@ namespace AqlaSerializer.unittest.Aqla
             [ProtoIgnore]
             public int SomeValue { get; set; }
 
+            [ProtoMember(1)]
+            public List<int> InnerList { get; set; }
+
             [ProtoAfterDeserialization]
             public void Callback()
             {
@@ -35,8 +38,10 @@ namespace AqlaSerializer.unittest.Aqla
             var tm = TypeModel.Create();
             tm.AutoCompile = true;
             tm.SkipCompiledVsNotCheck = true;
-            var v = tm.DeepClone(new Container() { Member = new TestList() });
+            var list = new List<int>() { 1, 2, 3 };
+            var v = tm.DeepClone(new Container() { Member = new TestList() { InnerList = list } });
             Assert.That(v.Member.SomeValue, Is.EqualTo(12345));
+            Assert.That(v.Member.InnerList, Is.EqualTo(list));
         }
     }
 }
