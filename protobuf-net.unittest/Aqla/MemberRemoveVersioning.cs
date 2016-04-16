11b04d34d7e6fe36f0a8ad9399dd210fbd2598d9
Vlad Taranov <wvlad.dev@gmail.com>
2016-04-16 18:24:59 +0300

    more complex test

--- protobuf-net.unittest/Aqla/MemberRemoveVersioning.cs
+++ protobuf-net.unittest/Aqla/MemberRemoveVersioning.cs
@@ -25,6 +25,10 @@ namespace AqlaSerializer.unittest.Aqla
 
             [SerializableMember(3)]
             public int Baz { get; set; }
+            
+            [SerializableMember(4)]
+            public CustomClass AnotherBar { get; set; }
+
         }
 
         [SerializableType(ImplicitFields = ImplicitFieldsMode.None, EnumPassthru = true)]
@@ -38,6 +42,9 @@ namespace AqlaSerializer.unittest.Aqla
 
             [SerializableMember(3)]
             public int Baz { get; set; }
+
+            [SerializableMember(4)]
+            public CustomClass AnotherBar { get; set; }
         }
 
         [SerializableType(ImplicitFields = ImplicitFieldsMode.None, EnumPassthru = true)]
@@ -48,51 +55,59 @@ namespace AqlaSerializer.unittest.Aqla
 
             [SerializableMember(3)]
             public int Baz { get; set; }
+
+            [SerializableMember(4)]
+            public CustomClass AnotherBar { get; set; }
         }
 
         [Test]
         public void ExecuteSimpleValue()
         {
             var tm = TypeModel.Create();
-            var f = tm.ChangeType<ContainerSimple, ContainerFinal>(new ContainerSimple() { Foo = 1, Baz = 2, Bar = new CustomClass() });
+            var f = tm.ChangeType<ContainerSimple, ContainerFinal>(new ContainerSimple() { Foo = 1, Baz = 2, Bar = new CustomClass(), AnotherBar = new CustomClass() { Property = 8 } });
             Assert.That(f.Foo, Is.EqualTo(1));
             Assert.That(f.Baz, Is.EqualTo(2));
+            Assert.That(f.AnotherBar.Property, Is.EqualTo(8));
         }
 
         [Test]
         public void ExecuteSimpleNull()
         {
             var tm = TypeModel.Create();
-            var f = tm.ChangeType<ContainerSimple, ContainerFinal>(new ContainerSimple() { Foo = 1, Baz = 2, Bar = null });
+            var f = tm.ChangeType<ContainerSimple, ContainerFinal>(new ContainerSimple() { Foo = 1, Baz = 2, Bar = null, AnotherBar = new CustomClass() { Property = 8 } });
             Assert.That(f.Foo, Is.EqualTo(1));
             Assert.That(f.Baz, Is.EqualTo(2));
+            Assert.That(f.AnotherBar.Property, Is.EqualTo(8));
         }
 
         [Test]
         public void ExecuteListValue()
         {
             var tm = TypeModel.Create();
-            var f = tm.ChangeType<ContainerList, ContainerFinal>(new ContainerList() { Foo = 1, Baz = 2, Bar = new List<CustomClass>() { new CustomClass() } });
+            var f = tm.ChangeType<ContainerList, ContainerFinal>(new ContainerList() { Foo = 1, Baz = 2, Bar = new List<CustomClass>() { new CustomClass() }, AnotherBar = new CustomClass() { Property = 8 } });
             Assert.That(f.Foo, Is.EqualTo(1));
             Assert.That(f.Baz, Is.EqualTo(2));
+            Assert.That(f.AnotherBar.Property, Is.EqualTo(8));
         }
 
         [Test]
         public void ExecuteListEmpty()
         {
             var tm = TypeModel.Create();
-            var f = tm.ChangeType<ContainerList, ContainerFinal>(new ContainerList() { Foo = 1, Baz = 2, Bar = new List<CustomClass>() });
+            var f = tm.ChangeType<ContainerList, ContainerFinal>(new ContainerList() { Foo = 1, Baz = 2, Bar = new List<CustomClass>(), AnotherBar = new CustomClass() { Property = 8 } });
             Assert.That(f.Foo, Is.EqualTo(1));
             Assert.That(f.Baz, Is.EqualTo(2));
+            Assert.That(f.AnotherBar.Property, Is.EqualTo(8));
         }
 
         [Test]
         public void ExecuteListNull()
         {
             var tm = TypeModel.Create();
-            var f = tm.ChangeType<ContainerList, ContainerFinal>(new ContainerList() { Foo = 1, Baz = 2, Bar = null });
+            var f = tm.ChangeType<ContainerList, ContainerFinal>(new ContainerList() { Foo = 1, Baz = 2, Bar = null, AnotherBar = new CustomClass() {Property = 8} });
             Assert.That(f.Foo, Is.EqualTo(1));
             Assert.That(f.Baz, Is.EqualTo(2));
+            Assert.That(f.AnotherBar.Property, Is.EqualTo(8));
         }
     }
 }
