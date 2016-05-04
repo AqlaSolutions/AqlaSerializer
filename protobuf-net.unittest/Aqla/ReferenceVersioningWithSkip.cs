785e95be4f11d2ccb42d16c5b01754cb3e1010d1
Vlad Taranov <wvlad.dev@gmail.com>
2016-05-04 22:09:55 +0300

    why size of serialized is not same as serialized original?

--- protobuf-net.unittest/Aqla/ReferenceVersioningWithSkip.cs
+++ protobuf-net.unittest/Aqla/ReferenceVersioningWithSkip.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using AqlaSerializer.Meta;
 using NUnit.Framework;
 
@@ -60,7 +61,34 @@ namespace AqlaSerializer.unittest.Aqla
             Assert.That(changed.SameBar, Is.Not.Null);
             Assert.That(changed.SameFoo, Is.Not.Null);
         }
-        
+
+        [Test]
+        public void SizeShouldBeSameFor3Passes()
+        {
+            var tm = TypeModel.Create();
+            var obj = new SourceComplex();
+            obj.Bar = obj.SameBar = new SourceBar();
+            obj.Bar.Foo = obj.SameFoo = new SimpleFoo();
+            using (var ms = new MemoryStream())
+            {
+                int? previous = null;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    tm.Serialize(ms, obj);
+                    ms.Position = 0;
+                    var cloned = tm.Deserialize<SourceComplex>(ms);
+                    var l = (int)ms.Length;
+                    if (previous != null)
+                        Assert.That(l, Is.EqualTo(previous.Value));
+                    previous = l;
+                    obj = cloned;
+                    ms.SetLength(0);
+                }
+
+            }
+        }
+
         // recursive: removed property inside removed property
         [SerializableType]
         public class SourceBar
