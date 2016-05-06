66be119c4820e1e2a38175b13ec4168fe1095025
Vlad Taranov <wvlad.dev@gmail.com>
2016-05-06 11:05:12 +0300

    added AOTCompatlyzer build step

--- protobuf-net.Extensions/Properties/AssemblyInfo.cs
+++ protobuf-net.Extensions/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.188")]
+[assembly: AssemblyVersion("2.0.0.190")]
 #if !CF
-[assembly: AssemblyFileVersion("2.0.0.188")]
+[assembly: AssemblyFileVersion("2.0.0.190")]
 #endif
 [assembly: CLSCompliant(true)]
