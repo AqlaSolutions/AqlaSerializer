4266fae9ff4d53210f8ccfab437c9045f293620d
Vladyslav Taranov <vtaranov@b2broker.com>
2022-05-18 00:42:17 +0300

    deflate example

---
+++ src/Examples/Deflate.cs
@@ -0,0 +1,42 @@
+﻿using AqlaSerializer.Meta;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    [TestFixture]
+    public class Deflate
+    {
+        [Test]
+        public void TestCompress()
+        {
+            var rtm = TypeModel.Create();
+            rtm.SkipCompiledVsNotCheck = true;
+
+            byte[] bytes;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (DeflateStream dest = new DeflateStream(stream, CompressionMode.Compress))
+                {
+                    rtm.Serialize(dest, "Test");
+                }
+
+                bytes = stream.ToArray();
+            }
+
+            using (var stream = new MemoryStream(bytes))
+            using (DeflateStream dest = new DeflateStream(stream, CompressionMode.Decompress))
+            {
+                var str = rtm.Deserialize<string>(dest);
+                Assert.AreEqual("Test", str);
+            }
+        }
+
+    }
+}
