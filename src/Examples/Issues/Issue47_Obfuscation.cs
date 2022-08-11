7cf4c93fedb18495da4fafa7f6c053d8e82b2ef6
jdsheneman <jdsheneman@users.noreply.github.com>
2022-08-11 14:39:52 -0500

    Update AddField method to throw exception when duplicate field and property names exist.  Added a test for this condition.

--- src/Examples/Issues/Issue47_Obfuscation.cs
+++ src/Examples/Issues/Issue47_Obfuscation.cs
@@ -8,16 +8,21 @@ namespace Examples.Issues
 {
     public class Issue47_Obfuscation
     {
-        [Test]
-        public void Test()
+        Assembly _assembly;
+        public Issue47_Obfuscation()
         {
 #if DEBUG
 
-            var assembly = Assembly.LoadFrom(@"..\..\..\..\..\assorted\Obfuscated\bin\Debug\Obfuscated.dll");
+            _assembly = Assembly.LoadFrom(@"..\..\..\..\..\assorted\Obfuscated\bin\Debug\Obfuscated.dll");
 #else
-            var assembly = Assembly.LoadFrom(@"..\..\..\..\..\assorted\Obfuscated\bin\Release\Obfuscated.dll");
+            _assembly = Assembly.LoadFrom(@"..\..\..\..\..\assorted\Obfuscated\bin\Release\Obfuscated.dll");
 #endif
-            Type obfuscatedType = assembly.GetType("a");
+        }
+
+        [Test]
+        public void TestDuplicateNames()
+        {
+            Type obfuscatedType = _assembly.GetType("a");
             var instance = Activator.CreateInstance(obfuscatedType);
             var membersWithTheSameName = obfuscatedType.GetMember("a", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
             Assert.AreEqual(4, membersWithTheSameName.Length);
@@ -44,6 +49,22 @@ namespace Examples.Issues
             Assert.IsNotNull(clone);
             var cloneFieldValue = field.GetValue(clone);
             Assert.AreEqual(cloneFieldValue, 8);
+        }
+
+        [Test]
+        public void TestDuplicateFieldAndPropertyNames()
+        {
+            Type obfuscatedType = _assembly.GetType("d");
+
+            var metaType = AqlaSerializer.Meta.RuntimeTypeModel.Default.Add(obfuscatedType, false);
+
+            var ex = Assert.Throws<ArgumentException>(() => {
+                metaType.AddField(metaType.GetNextFreeFieldNumber(), "d");
+            });
+
+            Assert.That(ex.Message.StartsWith("Unable to determine member: d"));
+            Assert.That(ex.Message.Contains("Parameter"));
+            Assert.That(ex.Message.Contains("memberName"));
         }
     }
 }
--- src/protobuf-net/Meta/MetaType.Fields.cs
+++ src/protobuf-net/Meta/MetaType.Fields.cs
@@ -177,15 +177,10 @@ namespace AqlaSerializer.Meta
         {
             if (Type.IsArray) throw new InvalidOperationException("Can't add fields to array type");
             MemberInfo mi = null;
-            mi = Type.GetField(memberName, Helpers.IsEnum(Type) ? BindingFlags.Static | BindingFlags.Public : BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            if (mi == null)
-            {
-                mi = Type.GetProperty(memberName, Helpers.IsEnum(Type) ? BindingFlags.Static | BindingFlags.Public : BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            }
-            if (mi == null)
-            {
-                throw new ArgumentException("Unable to determine member: " + memberName, nameof(memberName));
-            }
+            MemberInfo[] members = Type.GetMember(memberName, Helpers.IsEnum(Type) ? BindingFlags.Static | BindingFlags.Public : BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .Where(x => x.MemberType == MemberTypes.Field || x.MemberType == MemberTypes.Property).ToArray();
+            if (members != null && members.Length == 1) mi = members[0];
+            if (mi == null) throw new ArgumentException("Unable to determine member: " + memberName, nameof(memberName));
 
             Type miType;
 #if PORTABLE
